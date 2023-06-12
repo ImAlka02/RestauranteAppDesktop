0a47f094cd578902906c92faa40b859f340615e6
and01 <and01user@proton.me>
2023-06-11 18:39:16 -0600

    Catalogos again

--- ProyectoRestaurante/Catalogos/MenuCatalogo.cs
+++ ProyectoRestaurante/Catalogos/MenuCatalogo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoRestaurante.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace ProyectoRestaurante.Catalogos
 {
-    internal class MenuCatalogo
+    public class MenuCatalogo
     {
+        RestauranteContext context = new RestauranteContext();
+        public IEnumerable<Menu> GetMenus()
+        {
+            return context.Menu;
+        }
+        
+        public Menu ObtenerMenus(int Id)
+        {
+            return context.Menu.Where(x => x.Id == Id).FirstOrDefault();
+        }
+        
+        public void Delete(Menu a)
+        {
+            context.Menu.Remove(a);
+            context.SaveChanges();
+            context.Entry(a).Reload();
+        }
+        public void Update(Menu a)
+        {
+            context.Menu.Update(a);
+            context.SaveChanges();
+            context.Entry(a).Reload();
+        }
+        public void Create(Menu a)
+        {
+            context.Menu.Add(a);
+            context.SaveChanges();
+            context.Entry(a).Reload();
+        }
     }
 }
--- ProyectoRestaurante/Catalogos/PedidoCatalogo.cs
+++ ProyectoRestaurante/Catalogos/PedidoCatalogo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoRestaurante.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,44 @@ using System.Threading.Tasks;
 
 namespace ProyectoRestaurante.Catalogos
 {
-    internal class PedidoCatalogo
+    public class PedidoCatalogo
     {
+        RestauranteContext context = new RestauranteContext();
+        public IEnumerable<Detallespedido> GetDetalles()
+        {
+            return context.Detallespedido;
+        }
+        public IEnumerable<Pedido> GetPedidos()
+        {
+            return context.Pedido;
+        }
+
+        public Detallespedido ObtenerDetallesPedido(int Id)
+        {
+            return context.Detallespedido.Where(x => x.Id == Id).FirstOrDefault();
+        }
+        public Pedido ObtenerPedido(int Id)
+        {
+            return context.Pedido.Where(x => x.Id == Id).FirstOrDefault();
+        }
+     
+        public void Delete(Pedido a)
+        {
+            context.Pedido.Remove(a);
+            context.SaveChanges();
+            context.Entry(a).Reload();
+        }
+        public void Update(Pedido a)
+        {
+            context.Pedido.Update(a);
+            context.SaveChanges();
+            context.Entry(a).Reload();
+        }
+        public void Create(Pedido a)
+        {
+            context.Pedido.Add(a);
+            context.SaveChanges();
+            context.Entry(a).Reload();
+        }
     }
 }
