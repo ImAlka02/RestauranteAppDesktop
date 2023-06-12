e0cab12053d5d49899307f905dc6f7c93dad7f55
and01 <and01user@proton.me>
2023-06-11 19:32:45 -0600

    Actualizacion viewmodels

--- ProyectoRestaurante/ViewModels/MenuViewModel.cs
+++ ProyectoRestaurante/ViewModels/MenuViewModel.cs
@@ -1,12 +1,77 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using Microsoft.EntityFrameworkCore;
+using ProyectoRestaurante.Catalogos;
+using ProyectoRestaurante.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ProyectoRestaurante.ViewModels
 {
-    internal class MenuViewModel
+    public class MenuViewModel : INotifyPropertyChanged
     {
+        MenuCatalogo catalogoMen = new MenuCatalogo();
+        public ObservableCollection<Menu> listamenus { get; set; } = new ObservableCollection<Menu>();
+        public Menu? Menu { get; set; }
+        public string Error { get; set; }
+        public ICommand VerRegistrarMenuCommand { get; set; }
+        public ICommand VerEliminarMenuCommand { get; set; }
+        public ICommand VerEditarMenuCommand { get; set; }
+        public ICommand RegistrarMenuCommand { get; set; }
+        public ICommand EliminarMenuCommand { get; set; }
+        public ICommand EditarMenuCommand { get; set; }
+        public MenuViewModel()
+        {
+            VerRegistrarMenuCommand = new RelayCommand(VerRegistrarMenu);
+            VerEliminarMenuCommand = new RelayCommand(VerEliminarMenu);
+            VerEditarMenuCommand = new RelayCommand(VerEditarMenu);
+            RegistrarMenuCommand = new RelayCommand(RegistrarMenu);
+            EliminarMenuCommand = new RelayCommand(EliminarMenu);
+            EditarMenuCommand = new RelayCommand(EditarMenu);
+        
+
+        }
+
+        private void EditarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EliminarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RegistrarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void VerEditarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void VerEliminarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void VerRegistrarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Actualizar(string? propiedad = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propiedad));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
--- ProyectoRestaurante/ViewModels/PedidoViewModel.cs
+++ ProyectoRestaurante/ViewModels/PedidoViewModel.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using ProyectoRestaurante.Catalogos;
+using ProyectoRestaurante.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ProyectoRestaurante.ViewModels
 {
-    internal class PedidoViewModel
+    public class PedidoViewModel : INotifyPropertyChanged
     {
+        PedidoCatalogo catalogoPed = new PedidoCatalogo();
+        public ObservableCollection<Pedido> listpedidos { get; set; } = new ObservableCollection<Pedido>();
+        public Pedido? Pedido { get; set; }
+        public string Error { get; set; }
+        public ICommand VerRegistrarPedidoCommand { get; set; }
+        public ICommand VerEliminarPedidoCommand { get; set; }
+        public ICommand VerEditarPedidoCommand { get; set; }
+        public ICommand RegistrarPedidoCommand { get; set; }
+        public ICommand EliminarPedidoCommand { get; set; }
+        public ICommand EditarPedidoCommand { get; set; }
+        public PedidoViewModel()
+        {
+            VerRegistrarPedidoCommand = new RelayCommand(VerRegistrarMenu);
+            VerEliminarPedidoCommand = new RelayCommand(VerEliminarMenu);
+            VerEditarPedidoCommand = new RelayCommand(VerEditarMenu);
+            RegistrarPedidoCommand = new RelayCommand(RegistrarMenu);
+            EliminarPedidoCommand = new RelayCommand(EliminarMenu);
+            EditarPedidoCommand = new RelayCommand(EditarMenu);
+
+
+        }
+
+        private void EditarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EliminarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RegistrarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void VerEditarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void VerEliminarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void VerRegistrarMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Actualizar(string? propiedad = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propiedad));
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
