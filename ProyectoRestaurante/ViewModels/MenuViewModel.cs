328a8e395357e1251d56692406b562445592413a
ImAlka02 <ajiju.2000@hotmail.com>
2023-06-13 02:22:24 -0600

    Se cambiaron todas las vistas, el menu esta 70% terminado, falta pulir, tambien falta ver pedidos y sus estados, al igual que crear pedidos.

--- ProyectoRestaurante/ViewModels/MenuViewModel.cs
+++ ProyectoRestaurante/ViewModels/MenuViewModel.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using ProyectoRestaurante.Catalogos;
 using ProyectoRestaurante.Models;
+using ProyectoRestaurante.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -15,8 +16,17 @@ namespace ProyectoRestaurante.ViewModels
 {
     public class MenuViewModel : INotifyPropertyChanged
     {
+
+        private Accion operacion;
+
+        public Accion Operacion
+        {
+            get { return operacion; }
+            set { operacion = value; }
+        }
+
         MenuCatalogo catalogoMen = new MenuCatalogo();
-        public ObservableCollection<Menu> listamenus { get; set; } = new ObservableCollection<Menu>();
+        public ObservableCollection<Menu> ListaMenu { get; set; } = new ObservableCollection<Menu>();
         public Menu? Menu { get; set; }
         public string Error { get; set; }
         public ICommand VerRegistrarMenuCommand { get; set; }
@@ -27,13 +37,14 @@ namespace ProyectoRestaurante.ViewModels
         public ICommand EditarMenuCommand { get; set; }
         public MenuViewModel()
         {
+            operacion = Accion.VerMenu;
             VerRegistrarMenuCommand = new RelayCommand(VerRegistrarMenu);
             VerEliminarMenuCommand = new RelayCommand(VerEliminarMenu);
             VerEditarMenuCommand = new RelayCommand(VerEditarMenu);
             RegistrarMenuCommand = new RelayCommand(RegistrarMenu);
             EliminarMenuCommand = new RelayCommand(EliminarMenu);
             EditarMenuCommand = new RelayCommand(EditarMenu);
-        
+            ActualizarBD();
 
         }
 
@@ -67,10 +78,24 @@ namespace ProyectoRestaurante.ViewModels
             throw new NotImplementedException();
         }
 
+        private void ActualizarBD()
+        {
+            ListaMenu.Clear();
+            if(ListaMenu != null)
+            {
+                foreach (var item in catalogoMen.GetMenus())
+                {
+                    ListaMenu.Add(item);
+                }
+            }
+            
+        }
         void Actualizar(string? propiedad = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propiedad));
         }
+        
+
 
         public event PropertyChangedEventHandler? PropertyChanged;
     }
